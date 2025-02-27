09d6e896d4df7034a8709c0a699fba1d0e0797de
sameer <sameershaiksameer48@gmail.com>
2025-02-27 20:23:34 +0530

    range

--- .history/prime_20250227201935.py
+++ .history/prime_20250227201935.py
@@ -1,8 +1,8 @@
 start=int(input('enter the starting number:'))
 end=int(input('enter the last number:'))
 for i in range(start,end+1):
-    for j in range(2,(i//2)+1):
+    for j in range(2,(1//2)+1):
         if i%j==0:
-            break
+            print(i)
     else:
-        print(i)
+        continue
