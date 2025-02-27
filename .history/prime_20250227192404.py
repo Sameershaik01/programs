ed1ce5ebbbdb6d8894c2ab1b3a3f638a8bf0c2e3
sameer <sameershaiksameer48@gmail.com>
2025-02-27 20:14:31 +0530

    reducingtime

--- .history/prime_20250227192404.py
+++ .history/prime_20250227192404.py
@@ -1,11 +1,11 @@
 n=int(input('enter your number:'))
 i=1
 cnt=0
-while i<=(n//2):
+while i<=(n//2)+1:
     if n%i==0:
         cnt+=1
     i+=1
-if cnt==1:
+if cnt==2:
     print('prime')
 else:
     print('not prime')
--- .history/prime_20250227193657.py
+++ .history/prime_20250227193657.py
@@ -1,7 +1,7 @@
 n=int(input('enter your number:'))
 i=1
 cnt=0
-while i<=(n//2):
+while i<=(n//2)+1:
     if n%i==0:
         cnt+=1
     i+=1
