2ffd6a227b84219f9d8cbfc66e33175501aa3de7
sameer <sameershaiksameer48@gmail.com>
2025-02-27 23:12:20 +0530

    fibo1

---
+++ .history/fibo_20250227230937.py
@@ -0,0 +1,11 @@
+n=int(input('enter the number:'))
+a=0
+b=1
+i=1
+print(a)
+print(b)
+while i<=(n-2):
+    c=a+b
+    i=i+1
+    a=b
+    b=c
---
+++ fibo.py
@@ -0,0 +1,12 @@
+n=int(input('enter the number:'))
+a=0
+b=1
+i=1
+print(a)
+print(b)
+while i<=(n-2):
+    c=a+b
+    print(c)
+    i=i+1
+    a=b
+    b=c
